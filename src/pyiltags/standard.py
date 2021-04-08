04291abd0721d6ae0c83b41b695341534fcaa83c
Fabio Jun Takada Chino <gitea@fake.local>
2021-04-08 01:05:22 -0300

    Removing the unused dependency on codecs.

--- src/pyiltags/standard.py
+++ src/pyiltags/standard.py
@@ -29,8 +29,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import pyilint
-import codecs
-from typing import Generic
 from typing import List
 from .base import *
 
--- src/pyiltags/standard_tests.py
+++ src/pyiltags/standard_tests.py
@@ -29,6 +29,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from typing import Callable
+import codecs
 import unittest
 from .standard import *
 
